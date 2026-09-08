346637e1067ee5b66cb45d89be2b8ce8afb2d9ca
Цукер Богдан <bogdan.tsuker@gmail.com>
2026-09-08 15:42:14 +0300

    Начало работы

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-from src.tg_bot import main
+import asyncio
+from os import getenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+TOKEN = getenv("8679179719:AAG1ZGKS7OpaKutaao9iS6HdFkTFaRXERPE")
+
+dp = Dispatcher()
+
+# Обработчик запросов
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("ПРИВЕТ!")
+
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+    
